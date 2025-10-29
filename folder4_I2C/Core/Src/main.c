5571f69dd92179a8c3b75f20caa0b2bf8a5da99d
Haril Dave <harilhimanshudave@gmail.com>
2025-10-29 15:52:13 +0530

    playlist-1

---
+++ folder4_I2C/Core/Src/main.c
@@ -0,0 +1,353 @@
+/* USER CODE BEGIN Header */
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2025 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <stdio.h>
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+#define I2C_SCL_PIN 8
+#define I2C_SDA_PIN 9
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+UART_HandleTypeDef huart2;
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART2_UART_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+typedef struct {
+    I2C_TypeDef *Instance;
+    GPIO_TypeDef *GPIOx;
+    uint8_t SCL_Pin;
+    uint8_t SDA_Pin;
+    uint8_t AF;
+} I2C_Handle_t;
+
+void I2C_GPIO_Init(I2C_Handle_t *hi2c)
+{
+    uint32_t pos_scl = hi2c->SCL_Pin * 2;
+    uint32_t pos_sda = hi2c->SDA_Pin * 2;
+
+    if (hi2c->GPIOx == GPIOB) RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
+    if (hi2c->GPIOx == GPIOA) RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+    if (hi2c->GPIOx == GPIOC) RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
+
+    hi2c->GPIOx->MODER &= ~((3U << pos_scl) | (3U << pos_sda));
+    hi2c->GPIOx->MODER |= (2U << pos_scl) | (2U << pos_sda);
+    hi2c->GPIOx->OTYPER |= (1U << hi2c->SCL_Pin) | (1U << hi2c->SDA_Pin);
+    hi2c->GPIOx->OSPEEDR |= (3U << pos_scl) | (3U << pos_sda);
+    hi2c->GPIOx->PUPDR &= ~((3U << pos_scl) | (3U << pos_sda));
+    hi2c->GPIOx->PUPDR |= (1U << pos_scl) | (1U << pos_sda);
+
+    if (hi2c->SCL_Pin < 8)
+        hi2c->GPIOx->AFR[0] |= (hi2c->AF << (hi2c->SCL_Pin * 4));
+    else
+        hi2c->GPIOx->AFR[1] |= (hi2c->AF << ((hi2c->SCL_Pin - 8) * 4));
+
+    if (hi2c->SDA_Pin < 8)
+        hi2c->GPIOx->AFR[0] |= (hi2c->AF << (hi2c->SDA_Pin * 4));
+    else
+        hi2c->GPIOx->AFR[1] |= (hi2c->AF << ((hi2c->SDA_Pin - 8) * 4));
+}
+
+void I2C_Init(I2C_Handle_t *hi2c)
+{
+    if (hi2c->Instance == I2C1) RCC->APB1ENR |= RCC_APB1ENR_I2C1EN;
+    if (hi2c->Instance == I2C2) RCC->APB1ENR |= RCC_APB1ENR_I2C2EN;
+    if (hi2c->Instance == I2C3) RCC->APB1ENR |= RCC_APB1ENR_I2C3EN;
+
+    I2C_GPIO_Init(hi2c);
+
+    hi2c->Instance->CR1 = 0;
+    hi2c->Instance->CR2 = 16;
+    hi2c->Instance->CCR = 80;
+    hi2c->Instance->TRISE = 17;
+    hi2c->Instance->CR1 |= I2C_CR1_PE;
+}
+
+void I2C_Start(I2C_Handle_t *hi2c)
+{
+    hi2c->Instance->CR1 |= I2C_CR1_START;
+    while (!(hi2c->Instance->SR1 & I2C_SR1_SB));
+}
+
+void I2C_WriteAddr(I2C_Handle_t *hi2c, uint8_t addr)
+{
+    (void)hi2c->Instance->SR1;
+    hi2c->Instance->DR = addr << 1;
+    while (!(hi2c->Instance->SR1 & I2C_SR1_ADDR));
+    (void)hi2c->Instance->SR2;
+}
+
+void I2C_WriteData(I2C_Handle_t *hi2c, uint8_t data)
+{
+    while (!(hi2c->Instance->SR1 & I2C_SR1_TXE));
+    hi2c->Instance->DR = data;
+    while (!(hi2c->Instance->SR1 & I2C_SR1_BTF));
+}
+
+void I2C_Stop(I2C_Handle_t *hi2c)
+{
+    hi2c->Instance->CR1 |= I2C_CR1_STOP;
+}
+
+/* USER CODE END 0 */
+
+/**
+ * @brief  The application entry point.
+ * @retval int
+ */
+int main(void)
+{
+
+	/* USER CODE BEGIN 1 */
+
+	/* USER CODE END 1 */
+
+	/* MCU Configuration--------------------------------------------------------*/
+
+	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+	HAL_Init();
+
+	/* USER CODE BEGIN Init */
+
+	/* USER CODE END Init */
+
+	/* Configure the system clock */
+	SystemClock_Config();
+
+	/* USER CODE BEGIN SysInit */
+
+	/* USER CODE END SysInit */
+
+	/* Initialize all configured peripherals */
+	MX_GPIO_Init();
+	MX_USART2_UART_Init();
+	/* USER CODE BEGIN 2 */
+
+//	I2C1_GPIO_Init();
+//	I2C1_Init();
+//
+//	I2C1_Start();
+//	I2C1_WriteAddr(0x50);  /// any address we can write here
+//	I2C1_WriteData(0xAA);
+//	I2C1_Stop();
+	/* USER CODE END 2 */
+
+	/* Infinite loop */
+	/* USER CODE BEGIN WHILE */
+	while (1)
+	{
+		/* USER CODE END WHILE */
+
+		/* USER CODE BEGIN 3 */
+//		ret = HAL_I2C_IsDeviceReady(&hi2c1, i2c_address << 1, 2, 100);
+//
+//		if (ret == HAL_OK)
+//		{
+//			HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+//			HAL_Delay(1000);
+//		}
+//		else
+//		{
+//			HAL_Delay(100);
+//		}
+	}
+	/* USER CODE END 3 */
+}
+
+/**
+ * @brief System Clock Configuration
+ * @retval None
+ */
+void SystemClock_Config(void)
+{
+	RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+	/** Configure the main internal regulator output voltage
+	 */
+	__HAL_RCC_PWR_CLK_ENABLE();
+	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+	/** Initializes the RCC Oscillators according to the specified parameters
+	 * in the RCC_OscInitTypeDef structure.
+	 */
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+	RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+	RCC_OscInitStruct.PLL.PLLM = 16;
+	RCC_OscInitStruct.PLL.PLLN = 336;
+	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+	RCC_OscInitStruct.PLL.PLLQ = 4;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+	{
+		Error_Handler();
+	}
+
+	/** Initializes the CPU, AHB and APB buses clocks
+	 */
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+			|RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+	{
+		Error_Handler();
+	}
+}
+
+/**
+ * @brief USART2 Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_USART2_UART_Init(void)
+{
+
+	/* USER CODE BEGIN USART2_Init 0 */
+
+	/* USER CODE END USART2_Init 0 */
+
+	/* USER CODE BEGIN USART2_Init 1 */
+
+	/* USER CODE END USART2_Init 1 */
+	huart2.Instance = USART2;
+	huart2.Init.BaudRate = 115200;
+	huart2.Init.WordLength = UART_WORDLENGTH_8B;
+	huart2.Init.StopBits = UART_STOPBITS_1;
+	huart2.Init.Parity = UART_PARITY_NONE;
+	huart2.Init.Mode = UART_MODE_TX_RX;
+	huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+	huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+	if (HAL_UART_Init(&huart2) != HAL_OK)
+	{
+		Error_Handler();
+	}
+	/* USER CODE BEGIN USART2_Init 2 */
+
+	/* USER CODE END USART2_Init 2 */
+
+}
+
+/**
+ * @brief GPIO Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_GPIO_Init(void)
+{
+	GPIO_InitTypeDef GPIO_InitStruct = {0};
+	/* USER CODE BEGIN MX_GPIO_Init_1 */
+
+	/* USER CODE END MX_GPIO_Init_1 */
+
+	/* GPIO Ports Clock Enable */
+	__HAL_RCC_GPIOC_CLK_ENABLE();
+	__HAL_RCC_GPIOH_CLK_ENABLE();
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+	__HAL_RCC_GPIOB_CLK_ENABLE();
+
+	/*Configure GPIO pin Output Level */
+	HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+
+	/*Configure GPIO pin : B1_Pin */
+	GPIO_InitStruct.Pin = B1_Pin;
+	GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
+
+	/*Configure GPIO pin : LD2_Pin */
+	GPIO_InitStruct.Pin = LD2_Pin;
+	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+	HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+
+	/* USER CODE BEGIN MX_GPIO_Init_2 */
+
+	/* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
+void Error_Handler(void)
+{
+	/* USER CODE BEGIN Error_Handler_Debug */
+	/* User can add his own implementation to report the HAL error return state */
+	__disable_irq();
+	while (1)
+	{
+	}
+	/* USER CODE END Error_Handler_Debug */
+}
+#ifdef USE_FULL_ASSERT
+/**
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+	/* USER CODE BEGIN 6 */
+	/* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+	/* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
